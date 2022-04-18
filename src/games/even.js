fd22f80e368ec2db62036bddfb2549a42c138225
Nick Ponomarev <ponomnick@yandex.ru>
2022-04-18 16:48:09 +1000

    rewrite even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,26 +1,19 @@
-import {
-  randomNumber, greeting, getAnswer, checkAnswer,
-} from './index.js';
+import getRandomNumber from '../utils/generateRandomNumber.js';
+import runGame from '../index.js';
+
+const minNum = 1;
+const maxNum = 15;
+const description = 'Answer "yes" if the number is even, otherwise answer "no"';
 
 const isEven = (num) => (num % 2 === 0);
 
-const gameRun = () => {
-  const userName = greeting();
-  console.log('Answer "yes" if the number is even, otherwise answer "no"');
-  const countRounds = 3;
-  const minNum = 1;
-  const maxNum = 15;
-  for (let i = countRounds; i > 0; i -= 1) {
-    const num = randomNumber(minNum, maxNum);
-
-    const answer = getAnswer(num);
-    const trueAnswer = isEven(num) ? 'yes' : 'no';
-    const check = checkAnswer(trueAnswer, answer, userName);
-    if (!check) {
-      return;
-    }
-  }
-  console.log(`Congratulations, ${userName}!`);
+const getDataGame = () => {
+  const question = getRandomNumber(minNum, maxNum);
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
+  return [question, correctAnswer];
 };
 
-export default gameRun;
+const startGameEven = () => {
+  runGame(description, getDataGame);
+};
+export default startGameEven;
