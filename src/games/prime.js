bb4d5a18fef08d38f6e7d16ce66a73ee2d401855
Nick Ponomarev <ponomnick@yandex.ru>
2022-04-18 16:50:16 +1000

    rewrite prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,6 +1,9 @@
-import {
-  randomNumber, greeting, getAnswer, checkAnswer,
-} from './index.js';
+import getRandomNumber from '../utils/generateRandomNumber.js';
+import runGame from '../index.js';
+
+const minNum = 1;
+const maxNum = 15;
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
   if (num < 2) {
@@ -14,23 +17,13 @@ const isPrime = (num) => {
   return true;
 };
 
-const gameRun = () => {
-  const userName = greeting();
-  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-  const countRounds = 3;
-  const minNum = 1;
-  const maxNum = 15;
-  for (let i = countRounds; i > 0; i -= 1) {
-    const num = randomNumber(minNum, maxNum);
-
-    const answer = getAnswer(num);
-    const trueAnswer = isPrime(num) ? 'yes' : 'no';
-    const check = checkAnswer(trueAnswer, answer, userName);
-    if (!check) {
-      return;
-    }
-  }
-  console.log(`Congratulations, ${userName}!`);
+const getDataGame = () => {
+  const question = getRandomNumber(minNum, maxNum);
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
+  return [question, correctAnswer];
 };
 
-export default gameRun;
+const startGamePrime = () => {
+  runGame(description, getDataGame);
+};
+export default startGamePrime;
